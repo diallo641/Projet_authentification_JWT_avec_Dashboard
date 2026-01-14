b8c2cfbca4ac50d8360c8b69b9f9febfa452bc5d
Boubacar DIALLO <boubacar95@outlook.fr>
2026-01-14 17:16:48 +0000

    fichier qui permet de dire les roles autorisés

---
+++ Backend/middleware/autoriseroles.js
@@ -0,0 +1,10 @@
+// middlewares/authorizeRoles.js
+
+module.exports = (...roles) => {
+    return (req, res, next) => {
+        if (!req.user || !roles.includes(req.user.role)) {
+            return res.status(403).json({ message: "Accès refusé : rôle non autorisé" });
+        }
+        next();
+    };
+};
