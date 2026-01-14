25a2cb9bb938597c72384a7baf160b82231bc303
Boubacar DIALLO <boubacar95@outlook.fr>
2026-01-14 17:11:41 +0000

    mise à jour du fichier route pour les utilisateurs avec des limites d'accés selon le profil

--- Backend/routes/utilisateurs.js
+++ Backend/routes/utilisateurs.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/authentification.js');
+const authorizeRoles = require('../middleware/autoriseroles');
+
 const { ajouterutilisateur, allusers, user, editer, supprimer } = require('../controllers/utilisateurs');
-const router=express.Router();
 
-router.post("/ajouteruser", ajouterutilisateur);
-router.get("/Users", allusers);
-router.get("/User/:id", user);
-router.put("/editer/:id", editer);
-router.delete("/supprimer/:id", supprimer);
+// Routes Admin uniquement
+router.post("/ajouteruser", auth, authorizeRoles("Admin", "Manager"), ajouterutilisateur);
+router.put("/editer/:id", auth, authorizeRoles("Admin"), editer);
+router.delete("/supprimer/:id", auth, authorizeRoles("Admin"), supprimer);
+router.get("/Users", auth, authorizeRoles("Admin", "Manager"), allusers);
 
-module.exports= router;
+// Routes Admin, Manager, Client pour consulter un profil spécifique
+router.get("/User/:id", auth, authorizeRoles("Admin","Manager","Client"), user);
+
+module.exports = router;
