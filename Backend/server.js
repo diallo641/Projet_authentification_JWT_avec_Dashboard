31faec3bfe80b4f2ddce34bd0aa5eaa54daf6c61
Boubacar DIALLO <boubacar95@outlook.fr>
2026-01-14 17:05:02 +0000

    Mise en place du fichier server.js qui sera utilisé pour consomer les routes de notre API

---
+++ Backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port =5000;
+const connexionDB= require("./config/basededonnees");
+const cors = require("cors");
+app.use(cors());
+
+
+//Middleware 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+connexionDB();
+
+//Utiliser les endpoints
+app.use("/APIRole", require('./routes/roles'));
+app.use("/api/user", require("./routes/utilisateursJWT"));
+app.use("/apilogin", require("./routes/login"));
+
+
+//Ecouteur avec le port
+app.listen(port ,() => console.log("le serveur pour l'API authentification ave un dasboard a demaré aevc succés! et le port utilisé est :"+ port));
+
+
